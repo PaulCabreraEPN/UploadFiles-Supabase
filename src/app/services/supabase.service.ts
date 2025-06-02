48cb4fa5ce7618df6d285ab1474f8a6e001de7ea
Paul Sebastian Cabrera Cruz <151689119+PaulCabreraEPN@users.noreply.github.com>
2025-06-02 12:16:03 -0500

    Update supabase.service.ts

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -9,8 +9,8 @@ export class SupabaseService {
 
   constructor() {
     this.supabase = createClient(
-      'https://eabrubdlaywkxjsormzd.supabase.co',
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhYnJ1YmRsYXl3a3hqc29ybXpkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyOTc2MjAsImV4cCI6MjA2Mzg3MzYyMH0.uCp5AozdjUqgnBs9r2IgfXYrqe-BiXex5tr36Qfn55U'
+      'URL SUPABASE',
+      'CLAVE PUBLICA'
     );
   }
 
